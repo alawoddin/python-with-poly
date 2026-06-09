270cc5e40b2ca8844e73be2ac2213abeaf5fd1f1
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-09 16:00:40 +0430

    i create the ploy code

--- index.py
+++ index.py
@@ -1,3 +1,26 @@
-name = "alawoddin"
+# name = "alawoddin"
 
-print("hello " + name)
+# print("hello " + name)
+
+# class Car:
+#     def model(self):
+#         print("model 2024")
+
+# class bike:
+#      def model(self):
+#         print("model 2025")
+
+
+# for i in (Car() , bike()):
+#     i.model()
+
+class Python:
+    def Gen(self):
+        print("Gen 2")
+
+class Django:
+    def Gen(self):
+        print("Gen 3")
+
+for i in(Python() , Django()):
+    i.Gen()
